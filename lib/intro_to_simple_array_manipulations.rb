55309f1ee4e99d0b444557d94ea911158c19050e
Luis Bernal <luis_bernal8010@yahoo.com>
2020-04-08 18:04:24 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,7 @@
+def using_concat(all_my_favs, more_fav)
+  all_my_favs.concat(more_fav)
+end
+
+def using_insert(list_of_programing_languages, another_language)
+  list_of_programing_languages.insert(4,another_language)
+end
